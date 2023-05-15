3d672d16ae963ad504451d7a19d7ce977d8a24f2
HaiderMalik12 <haidermalik504@gmail.com>
2023-05-15 15:51:31 +0500

    one to many and many to one relation

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,8 @@ import { SongsModule } from './songs/songs.module';
 import { Song } from './songs/song.entity';
 import { Artist } from './artists/artist.entity';
 import { User } from './users/user.entity';
+import { Playlist } from './playlists/playlist.entity';
+import { PlayListModule } from './playlists/playlists.module';
 // import { DataSource } from 'typeorm';
 
 @Module({
@@ -19,10 +21,11 @@ import { User } from './users/user.entity';
       port: 5432,
       username: 'postgres',
       password: 'root',
-      entities: [Song, Artist, User],
+      entities: [Song, Artist, User, Playlist],
       synchronize: true,
     }),
     SongsModule,
+    PlayListModule,
   ],
   controllers: [AppController],
   providers: [AppService],
