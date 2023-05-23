3486d7c463a78cf7defcbca71805d9e8dfa70663
HaiderMalik12 <haidermalik504@gmail.com>
2023-05-23 20:33:31 +0500

    authenticate with passport jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,23 +3,30 @@ import { UsersService } from 'src/users/users.service';
 import { LoginDTO } from './dto/login.dto';
 import { User } from 'src/users/user.entity';
 import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UsersService) {}
+  constructor(
+    private userService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
-  async login(loginDTO: LoginDTO): Promise<User> {
+  async login(loginDTO: LoginDTO): Promise<{ accessToken: string }> {
     const user = await this.userService.findOne(loginDTO); // 1.
 
     const passwordMatched = await bcrypt.compare(
       loginDTO.password,
       user.password,
-    ); // 2.
+    );
 
     if (passwordMatched) {
-      //3
-      delete user.password; // 4.
-      return user;
+      delete user.password;
+      const payload = { email: user.email, sub: user.id };
+
+      return {
+        accessToken: this.jwtService.sign(payload),
+      };
     } else {
       throw new UnauthorizedException('Password does not match'); // 5.
     }
