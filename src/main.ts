86775e6b608271ad5585020070a08a523b3dbd80
HaiderMalik12 <haidermalik504@gmail.com>
2023-06-01 16:48:35 +0500

    completed custom configuration

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SeedService } from './seed/seed.service';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,7 @@ async function bootstrap() {
    */
   // const seedService = app.get(SeedService);
   // await seedService.seed();
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get<number>('port'));
 }
 bootstrap();
